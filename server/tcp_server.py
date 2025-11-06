f14ab803671f33906805bae6559c1c6b7f03a641
71-hino-matutaro <71-hino-matutaro@g.ecc.u-tokyo.ac.jp>
2025-11-06 15:26:22 +0900

    サーバーにメッセージを複数受け取れるよう変更

--- server/tcp_server.py
+++ server/tcp_server.py
@@ -4,22 +4,28 @@
 
 import socket
 import threading
+
+#IPアドレスとポートを指定
 bind_ip = '0.0.0.0'
 bind_port = 10145
+
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 server.bind((bind_ip,bind_port))
 server.listen(5)
 print(f"[*] listen {bind_ip}:{bind_port}")
+
+#
 def handle_client(client_socket):
     bufsize=1024
-    request = client_socket.recv(bufsize)
-    print(f"[*] recv: {request.decode('utf-8')}")
-    client_socket.send('Hey Client!\n'.encode('utf-8'))
-    client_socket.close()
+    while True:
+        request = client_socket.recv(bufsize)
+        print(f"[*] recv: {request.decode('utf-8')}")
+        client_socket.send('Received Message!\n'.encode('utf-8'))
+
 
 while True:
     client,addr = server.accept()
     print(f"[*] connected from: {addr[0],addr[1]}")
-    client_handler = threading.Thread(target=handle_client,args=(client,))
+    client_handler = threading.Thread(target=handle_client,args=(client,)) #スレッドを立てることで複数のクライアントからの接続に対応
     client_handler.start()
