c7c90da27414e2f19e1ba09238d4034334d504eb
71-hino-matutaro <71-hino-matutaro@g.ecc.u-tokyo.ac.jp>
2025-11-11 14:16:30 +0900

    チャタリング対策/ボタンを押したときのみ戻り値

--- ESP8266_client/関数セット/detectPushSWON.ino
+++ ESP8266_client/関数セット/detectPushSWON.ino
@@ -1,7 +1,10 @@
 boolean detectPushSWON() {
     static boolean prev_sw_status = false;
-    boolean current_sw_status = digitalRead(2); 
+    boolean current_sw_status = !digitalRead(2); 
     boolean push_detected = (prev_sw_status == false) && (current_sw_status == true);
     prev_sw_status = current_sw_status;
-    return push_detected;
+    if(push_detected){
+        delay(100); //チャタリング対策
+        return push_detected;
+    }
 }
