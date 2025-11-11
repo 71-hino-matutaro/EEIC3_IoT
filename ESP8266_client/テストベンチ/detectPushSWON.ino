005eed9555026827887af8cb49bca73ae2869f8a
71-hino-matutaro <71-hino-matutaro@g.ecc.u-tokyo.ac.jp>
2025-11-11 14:28:17 +0900

    detectPushSWONのテストベンチ

--- ESP8266_client/テストベンチ/detectPushSWON.ino
+++ ESP8266_client/テストベンチ/detectPushSWON.ino
@@ -1,4 +1,17 @@
-boolean detectPushSWON() {
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(2,INPUT);
+}
+
+void loop() {
+  //push状態での初回呼び出し
+  Serial.println(detectPushSWON());
+  delay(500);
+}
+
+//以下関数の定義
+boolean detectPushSWON(){
     static boolean prev_sw_status = false;
     boolean current_sw_status = !digitalRead(2); 
     boolean push_detected = (prev_sw_status == false) && (current_sw_status == true);
