ad8cf0d989fae1ea97c8e2092f87de453d9f9860
71-hino-matutaro <71-hino-matutaro@g.ecc.u-tokyo.ac.jp>
2025-11-13 13:39:00 +0900

    gitIlluminance関数のテストベンチを追加

---
+++ ESP8266_client/テストベンチ/getIlluminance.ino
@@ -0,0 +1,19 @@
+void setup(){
+    Serial.begin(9600);
+    pinMode(A0,INPUT);
+}
+
+void loop(){
+    Serial.println(analogRead(A0));
+    Serial.println(getIlluminance());
+    delay(1000);
+}
+
+float getIlluminance(){
+    int t = analogRead(A0);
+    float v = t/320.0; //抵抗電圧を求める
+    float i = v/0.997; //mA単位にする 並列抵抗は0.997Ω
+    float logi = log10(i);
+    float k = ((4+log10(2.0))/(4+log10(5.0/3.0)));//傾き
+    return pow(10,k*(logi+4-log10(3))-1);
+}
