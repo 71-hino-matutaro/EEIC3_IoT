782204ffa9ef0a540fc8198b7054e0c9c0805e99
71-hino-matutaro <71-hino-matutaro@g.ecc.u-tokyo.ac.jp>
2025-11-13 13:37:45 +0900

    getIlluminance関数を実装

--- ESP8266_client/関数セット/getIlluminance.ino
+++ ESP8266_client/関数セット/getIlluminance.ino
@@ -1,3 +1,8 @@
-int getIlluminance(){
-    まだわからん
+float getIlluminance(){
+    int t = analogRead(A0);
+    float v = t/320.0; //抵抗電圧を求める
+    float i = v/0.997; //mA単位にする 並列抵抗は0.997Ω
+    float logi = log10(i);
+    float k = ((4+log10(2.0))/(4+log10(5.0/3.0)));//傾き
+    return pow(10,k*(logi+4-log10(3))-1);
 }
